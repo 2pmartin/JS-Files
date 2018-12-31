e0f001456d8530ed95a9845dd464e6164dd4b5b6
2pmartin <46133538+2pmartin@users.noreply.github.com>
2018-12-31 10:12:27 -0800

    Add files via upload

---
+++ Proto1/sketch.js
@@ -0,0 +1,16 @@
+
+// function Particle(){
+//   this.x = 100;
+//   this.y = 99;
+// }
+
+//var = new Particle();
+
+
+function setup() {
+
+  }
+
+function draw() {
+  creatCanvas(400, 400);// put drawing code here
+}
